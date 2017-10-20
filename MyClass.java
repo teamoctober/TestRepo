0ada1a2b52b3e10e44e69105e93d03038ea86148
teamoctober <teamoctober@users.noreply.github.com>
2017-10-20 14:18:41 +0200

    Student class with the students information

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+public class Myclass
+{
+	int ID = 0; 
+	string Name = "Jack"; 
+	int student# = 0;
+	
+	public static void (String [] args)
+	{
+		for(int i =0; i<0; i++)
+		{
+			system.out.println(i); 
+		}
+	}
+
+}
